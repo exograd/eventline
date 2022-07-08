4941ab1d83a1b30f266a731ada08db3630cf05bd
Nicolas Martyanoff <nicolas.martyanoff@exograd.com>
2022-07-08 13:18:50 +0200

    implement notification creation

--- pkg/service/notifications.go
+++ pkg/service/notifications.go
@@ -1,6 +1,15 @@
 package service
 
-import "github.com/exograd/go-daemon/check"
+import (
+	"bytes"
+	"fmt"
+	"path"
+	"time"
+
+	"github.com/exograd/eventline/pkg/eventline"
+	"github.com/exograd/go-daemon/check"
+	"github.com/exograd/go-daemon/pg"
+)
 
 type NotificationsCfg struct {
 	SMTPServer    *SMTPServerCfg `json:"smtp_server"`
@@ -32,4 +41,48 @@ func DefaultNotificationsCfg() *NotificationsCfg {
 		FromAddress:   "no-reply@localhost",
 		SubjectPrefix: "[eventline] ",
 	}
+}
+
+func (s *Service) CreateNotification(conn pg.Conn, recipients []string, templateName string, templateData interface{}, scope eventline.Scope) error {
+	projectId := scope.(*eventline.ProjectScope).ProjectId
+
+	now := time.Now().UTC()
+
+	message, err := s.RenderNotificationMessage(templateName, templateData)
+	if err != nil {
+		return fmt.Errorf("cannot render message: %w", err)
+	}
+
+	notification := eventline.Notification{
+		Id:               eventline.GenerateId(),
+		ProjectId:        projectId,
+		Recipients:       recipients,
+		Message:          message,
+		NextDeliveryTime: now,
+		DeliveryDelay:    0,
+	}
+
+	if err := notification.Insert(conn); err != nil {
+		return fmt.Errorf("cannot insert notification: %w", err)
+	}
+
+	return nil
+}
+
+func (s *Service) RenderNotificationMessage(name string, data interface{}) ([]byte, error) {
+	name = path.Join("notifications", name)
+
+	obj := struct {
+		Data interface{}
+	}{
+		Data: data,
+	}
+
+	var buf bytes.Buffer
+
+	if err := s.TextTemplate.ExecuteTemplate(&buf, name, obj); err != nil {
+		return nil, err
+	}
+
+	return buf.Bytes(), nil
 }
