f3088625effde13e6b9023fde9becbe01e100750
Julien Dessaux <julien.dessaux@adyxax.org>
2023-09-21 23:55:05 +0200

    Add an api route to get an identity from its id

--- pkg/service/api_http_server_identities.go
+++ pkg/service/api_http_server_identities.go
@@ -10,6 +10,8 @@ import (
 func (s *APIHTTPServer) setupIdentityRoutes() {
 	s.route("/identities", "GET", s.hIdentitiesGET,
 		HTTPRouteOptions{Project: true})
+	s.route("/identities/id/:id", "GET", s.hIdentitiesIdGET,
+		HTTPRouteOptions{Project: true})
 }
 
 func (s *APIHTTPServer) hIdentitiesGET(h *HTTPHandler) {
@@ -35,4 +37,18 @@ func (s *APIHTTPServer) hIdentitiesGET(h *HTTPHandler) {
 	}
 
 	h.ReplyJSON(200, page)
+}
+
+func (s *APIHTTPServer) hIdentitiesIdGET(h *HTTPHandler) {
+	identityId, err := h.IdPathVariable("id")
+	if err != nil {
+		return
+	}
+
+	identity, err := s.LoadIdentity(h, identityId)
+	if err != nil {
+		return
+	}
+
+	h.ReplyJSON(200, identity)
 }
